e76ddc14bdbff73e317a28925f8f1a764387135c
xposure <chris7333@yahoo.com>
2014-07-25 01:09:42 -0400

    blood

--- Source/Samples/Atma.Samples.BulletHell/World/Map.cs
+++ Source/Samples/Atma.Samples.BulletHell/World/Map.cs
@@ -18,22 +18,25 @@ namespace Atma.Samples.BulletHell.World
 
         //private Generators.BaseLevel _baselevel;
         private Generators.ILevel _baselevel;
-        private int scale = 25;
+        private int scale = 55;
         private SpriteBatch2 batch;
         private Texture2D texture;
+        private Texture2D bloodTexture;
         //private Random random = new Random();
+
+
 
         public void renderOpaque()
         {
             //return;
             //random = new Random(123);
             var offset = new Vector2(scale * _baselevel.Width, scale * _baselevel.Height) * 0.5f;
-            
+
             PerformanceMonitor.start("render map tiles");
             var worldBounds = GameWorld.instance.currentCamera.worldBounds;
             worldBounds.Inflate(Vector2.One * 2 * scale);
             var min = getMapPoint(worldBounds.Minimum + offset);
-            var max = getMapPoint(worldBounds.Maximum +offset);
+            var max = getMapPoint(worldBounds.Maximum + offset);
 
             batch.Begin(Microsoft.Xna.Framework.Graphics.SpriteSortMode.Deferred);
             for (var w = min.X; w < max.X; w++)
@@ -54,13 +57,29 @@ namespace Atma.Samples.BulletHell.World
                         case CellType.ENTRANCE:
                             //batch.draw(texture, p, new Vector2(scale, scale), color: Color.Green);
                             //break;
+                            //var west = _baselevel.GetCell(w - 1, h) == CellType.ROOM;
+                            //var north = _baselevel.GetCell(w, h - 1) == CellType.ROOM;
+                            //var east = _baselevel.GetCell(w + 1, h) == CellType.ROOM;
+                            //var south = _baselevel.GetCell(w, h + 1) == CellType.ROOM;
+
+                            //var index = 0;
+                            //if (west) index |= 1;
+                            //if (north) index |= 2;
+                            //if (east) index |= 4;
+                            //if (south) index |= 8;
+
+                            //var src = AxisAlignedBox.Null;
+
+                            //switch(index)
+
                             var colorSpread = 10;
                             var c1 = new Color(211 - colorSpread, 211 - colorSpread, 211 - colorSpread, 255);
                             var c2 = new Color(211 + colorSpread, 211 + colorSpread, 211 + colorSpread, 255);
                             //var r = random.NextFloat();
                             var r = PerlinSimplexNoise.noise(w, h);
 
-                            batch.draw(texture, p-offset, new Vector2(scale, scale), color: Color.Lerp(c1, c2, r));
+                            batch.draw(texture, p - offset, new Vector2(scale, scale), color: Color.Lerp(c1, c2, r));
+
                             break;
                         case CellType.EMPTY:
                             //batch.draw(texture, p, new Vector2(scale, scale), color: Color.BlanchedAlmond, depth: 0.1f);
@@ -82,7 +101,56 @@ namespace Atma.Samples.BulletHell.World
 
         public void renderAlphaBlend()
         {
+            var offset = new Vector2(scale * _baselevel.Width, scale * _baselevel.Height) * 0.5f;
 
+            PerformanceMonitor.start("render map tiles");
+            var worldBounds = GameWorld.instance.currentCamera.worldBounds;
+            worldBounds.Inflate(Vector2.One * 2 * scale);
+            var min = getMapPoint(worldBounds.Minimum + offset);
+            var max = getMapPoint(worldBounds.Maximum + offset);
+
+            batch.Begin(Microsoft.Xna.Framework.Graphics.SpriteSortMode.Deferred);
+            for (var w = min.X; w < max.X; w++)
+            {
+                for (var h = min.Y; h < max.Y; h++)
+                {
+                    var p = new Vector2(w, h) * scale;
+                    var cell = _baselevel.GetCell(w, h);
+
+                    switch (cell)
+                    {
+                        case CellType.ROOM:
+                        case CellType.CORRIDOR:
+                        case CellType.ENTRANCE:
+                            var colorSpread = 10;
+                            var c1 = new Color(211 - colorSpread, 211 - colorSpread, 211 - colorSpread, 255);
+                            var c2 = new Color(211 + colorSpread, 211 + colorSpread, 211 + colorSpread, 255);
+
+
+                            var blood = PerlinSimplexNoise.noise(w * 0.25f, h * 0.25f);
+                            if (blood < 0.1999999f)
+                            {
+                                var index = (int)(blood * 75);
+                                var x = index % 5;
+                                var y = (index - x) / 5;
+
+                                var srcScale = 32;
+                                var src = AxisAlignedBox.FromRect(new Vector2(x, y) * srcScale, Vector2.One * srcScale);
+
+                                batch.draw(bloodTexture, p - offset + new Vector2(scale,scale) * 0.25f, new Vector2(scale, scale)*0.5f, sourceRectangle: src, origin: new Vector2(0.5f, 0.5f), color: Color.White * 0.5f);//, color: Color.Lerp(c1, c2, r));
+                            }
+
+                            break;
+                        case CellType.EMPTY:
+                        case CellType.PERIMITER:
+                        case CellType.WALL:
+                            break;
+                    }
+                }
+            }
+
+            batch.End();
+            PerformanceMonitor.end("render map tiles");
         }
 
         public void renderOverlay()
@@ -98,7 +166,7 @@ namespace Atma.Samples.BulletHell.World
         {
             //_baselevel.Generate(123);
 
-            _baselevel = new Generators.JWLevelGenerator(150,150);
+            _baselevel = new Generators.JWLevelGenerator(150, 150);
             //_baselevel = new Generators.Dungeon(100, 100, 4, 8, 4, 8);
             //_baselevel = new Generators.Town(100, 100);
             //_baselevel = new Generators.StringLevel();
@@ -106,6 +174,7 @@ namespace Atma.Samples.BulletHell.World
 
             batch = new SpriteBatch2(display.device);
             texture = assets.getTexture("bullethell:tiles/plain2");// new Texture2D("floor:texture", TextureData.create(1, 1, Microsoft.Xna.Framework.Color.White));
+            bloodTexture = assets.getTexture("bullethell:bullethell/blood2");// new Texture2D("floor:texture", TextureData.create(1, 1, Microsoft.Xna.Framework.Color.White));
         }
 
         public AxisAlignedBox getCellAABBFromWorld(Vector2 p)
@@ -157,7 +226,8 @@ namespace Atma.Samples.BulletHell.World
             {
                 var jw = (Generators.JWLevelGenerator)_baselevel;
                 stepDelayTimer -= delta;
-                while(stepDelayTimer < 0f){
+                while (stepDelayTimer < 0f)
+                {
                     stepDelayTimer += stepDelay;
                     if (!jw.step())
                     {
