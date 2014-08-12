46fcd2585ce3d3153ef4c4136c238a5c342da49b
Chris Phillips <chris.phillips@mindmatrix.net>
2014-08-12 10:21:34 -0400

    aspect

--- Source/Core/Atma.Framework/2_0/Aspect.cs
+++ Source/Core/Atma.Framework/2_0/Aspect.cs
@@ -2,20 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Atma.Core;
 
 namespace Atma._2_0
 {
     public class Aspect
     {
-        private static uint _thingIndex = 0;
+        private static Logger _logger = Logger.getLogger(typeof(Aspect));
+        
+        private static uint _aspectIndex = 0;
+        private static Dictionary<uint, Aspect> _aspects = new Dictionary<uint, Aspect>();
+        private static HashSet<uint> _delayDestroy = new HashSet<uint>();
+
         public readonly uint id;
 
         public HideFlags hideFlags;
 
         protected Aspect()
         {
-            id = _thingIndex++;
+            id = _aspectIndex++;
             hideFlags = HideFlags.None;
+        }
+
+        public void destroy()
+        {
+            _delayDestroy.Add(this.id);
+        }
+
+        public void destroyNow()
+        {
+            destroyNow(this);
         }
 
         public override int GetHashCode()
@@ -26,6 +42,38 @@ namespace Atma._2_0
         public override bool Equals(object obj)
         {
             return base.Equals(obj as Aspect);
+        }
+
+        internal static void processDestroyed()
+        {
+            while (_delayDestroy.Count > 0)
+            {
+                //lets do this in case somehow destroy creates new destroys
+                var list = _delayDestroy.ToArray();
+                _delayDestroy.Clear();
+
+                foreach (var id in list)
+                {
+                    Aspect aspect;
+                    if (!_aspects.TryGetValue(id, out aspect))
+                        _logger.warn("aspect {0} was marked for destroy but was missing", id);
+
+                    destroyNow(aspect);
+                }
+            }
+        }
+
+        internal static void destroyNow(Aspect aspect)
+        {
+            if (aspect)
+                _logger.warn("tried to destroy a null aspect");
+            else
+            {
+                _aspects.Remove(aspect.id);
+
+                //needed in case destroy was called and then destroyNow - were going to let the warnings catch it
+                //_delayDestroy.Remove(aspect.id);
+            }
         }
 
         public static bool Compare(Aspect left, Aspect right)
@@ -43,10 +91,12 @@ namespace Atma._2_0
         {
             return !Aspect.Compare(exists, null);
         }
+
         public static bool operator ==(Aspect x, Aspect y)
         {
             return Aspect.Compare(x, y);
         }
+
         public static bool operator !=(Aspect x, Aspect y)
         {
             return !Aspect.Compare(x, y);
